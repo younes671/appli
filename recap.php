c9511cbc2e3a8bb3fb6e355a489365726af681d7
younes671@live.fr <you@example.com>
2024-02-06 14:02:13 +0100

    commit ajout class bootstrap

--- recap.php
+++ recap.php
@@ -7,6 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Récapitulatif des produits</title>
 </head>
 <body>
@@ -16,7 +18,7 @@
         if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
             echo "<p>Aucun produit en session...</p>";
         }else{
-            echo "<table>",
+            echo "<table class='table table-striped'>",
                         "<thead>",
                             "<tr>",
                                 "<th>#</th>",
